a8fe057a6b3c1230a1f72fedac209ec01c75490b
dineshelan <dineshelan99@gmail.com>
2024-02-16 10:39:24 +0530

    First Commit

---
+++ src/main/java/com/testapp/controller/TestAppController.java
@@ -0,0 +1,13 @@
+package com.testapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestAppController {
+	@GetMapping(value = "/getMsg")
+	public String getMsg() {
+		return " Hello World";
+	}
+
+}
